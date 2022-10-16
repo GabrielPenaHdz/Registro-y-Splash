a6efb4ce7fe7ab3d2a0ea59964eeaefcfccbed6c
GabrielPenaHdz <pena.hernandez.gabriel15@gmail.com>
2022-10-15 23:41:58 -0500

    Olvide

---
+++ app/src/main/java/com/example/splashbueno/OlvidePass.java
@@ -0,0 +1,14 @@
+package com.example.splashbueno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OlvidePass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvide_pass);
+    }
+}
